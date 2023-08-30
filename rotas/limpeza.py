ca9e1a286371a20865d08b85e103f57f8fca4aa7
Júlio Cascalles <julio.cascalles@outlook.com>
2023-08-30 11:34:55 -0300

    [Refatoração] Docstring na rota limpar_inativos

--- rotas/limpeza.py
+++ rotas/limpeza.py
@@ -8,6 +8,17 @@ router = APIRouter()
 
 @router.delete(EXCLUIR_HOSPEDES_INATIVOS)
 def limpar_inativos():
+    """
+    Todos os hóspedes que já deixaram seus hotéis
+    e não estão mais usando os serviços da API
+    são marcados com STATUS_INATIVO (0).
+
+    A **limpeza de inativos** remove esses registros
+    do banco de dados e devolve os nomes das pessoas
+    que foram excluídas.
+    
+    > Caso não encontre todos os inativos, retorna um erro.
+    """
     nomes = [h.nome for h in Hospede.find(status=STATUS_INATIVO)]
     total = Hospede.delete(status=STATUS_INATIVO).deleted_count
     if total != len(nomes):
